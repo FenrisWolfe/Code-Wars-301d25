177870b96e7cb3e9e06b0303b7e106771b585b05
FREDRIC THOMAS RAPP <fred.rapp@gmail.com>
2017-10-25 18:41:33 -0700

    completed solution added image and comments in readme

--- insert-dashes/solution.js
+++ insert-dashes/solution.js
@@ -1,1 +1,18 @@
+function insertDash(num) {
+  //code me
+  var n = num.toString();
+  var answer = '';
+  var i = 0;
+  while (i < n.length) {
 
+    if (parseInt(n[i]) % 2 !== 0 && parseInt(n[i + 1]) % 2 !== 0 && i !== n.length - 1) {
+      answer = answer + n[i] + '-';
+    } else if (parseInt(n[i]) % 2 !== 0 && parseInt(n[i + 1]) % 2 !== 0 && i === n.length - 1) {
+      answer = answer + n[i];
+    } else {
+      answer = answer + n[i];
+    }
+    i++;
+  }
+  return answer;
+}
