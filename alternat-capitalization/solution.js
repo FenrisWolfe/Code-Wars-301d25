33fc2ea06793c036fc774a3487757f64c5aa2d58
FREDRIC THOMAS RAPP <fred.rapp@gmail.com>
2017-10-30 00:41:51 -0700

    solution to alternate-capitalization

---
+++ alternat-capitalization/solution.js
@@ -0,0 +1,17 @@
+function capitalize(s){
+  var arr = s.split('');
+  var arrOdd = [];
+  arrOdd.push(arr[0]);
+  arr[0] = arr[0].toUpperCase();
+  for (var i = 1; i < arr.length;i++) {
+    if (i%2 === 0) {
+      arrOdd.push(arr[i]);
+      arr[i] = arr[i].toUpperCase();
+    } else {
+      arrOdd[i] = arr[i].toUpperCase();
+    }
+  }
+  var sEven = arr.join('');
+  var sOdd = arrOdd.join('');
+  return [sEven, sOdd];
+};
